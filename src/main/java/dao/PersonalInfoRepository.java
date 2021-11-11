437e975eb43b0604aff792a79a7137cceadb9dba
McFlurry69 <Cten95@mail.ru>
2021-11-11 20:26:33 +0300

    remove master

--- src/main/java/dao/PersonalInfoRepository.java
+++
@@ -1,9 +0,0 @@
-package dao;
-
-import model.PersonalInfo;
-
-public abstract class PersonalInfoRepository extends GeneralRepositoryImplementation<PersonalInfo> {
-    public PersonalInfoRepository(Class<PersonalInfo> typeParameterClass) {
-        super(PersonalInfo.class);
-    }
-}
